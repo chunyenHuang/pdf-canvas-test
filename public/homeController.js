a900b7f524c25248aade5458e4f4c87d3485e257
John Huang <little78926@gmail.com>
2016-05-12 14:34:16 -0700

    I add different ways to save canvas.#1

--- public/homeController.js
+++ public/homeController.js
@@ -23,9 +23,71 @@ function home($http, $scope, $location, addService) {
   $scope.get = function () {
     pastePDF($scope.linkUrl, 'pdf-field');
   }
+
   $scope.default = function (num) {
     $scope.linkUrl = 'https://s3.amazonaws.com/pdf-canvas-test/test'+ num + '.pdf';
     pastePDF($scope.linkUrl, 'pdf-field');
+  }
+
+  $scope.saveToImg = function () {
+    var back = document.getElementById('theImg');
+    var drawing = document.getElementById('canvas');
+
+    var newCan = document.createElement('canvas');
+    newCan.className = 'hidden';
+    newCan.setAttribute('id', 'newCan');
+    newCan.width = 890;
+    newCan.height = 690;
+    var content = newCan.getContext('2d');
+    content.drawImage(back, 0, 0, 890, 690);
+    content.drawImage(drawing, 0, 0, 890, 690);
+    document.body.appendChild(newCan);
+    newCan.toBlob(function(blob) {
+      saveAs(blob, "newCan.png");
+    });
+  }
+
+  $scope.saveToImgHtml = function () {
+    html2canvas($("#box"), {
+      allowTaint: true,
+      logging:true,
+      onrendered: function(newCanvas) {
+        document.body.appendChild(newCanvas);
+        newCanvas.className = 'hidden';
+        newCanvas.toBlob(function(blob) {
+          saveAs(blob, "new.png");
+        });
+      }
+    });
+  }
+
+  $scope.saveAsPdf = function() {
+    html2canvas($("#box"), {
+      allowTaint: true,
+      logging:true,
+      onrendered: function(newCanvas) {
+        var data = newCanvas.toDataURL("image/png");
+        var doc = {
+          content: [
+            { text: 'PDF Canvas Test', style: 'header' },
+            {
+              image: data,
+              width: 450,
+              margin: [ 0, 0, 0, 0 ],
+            },
+          ]
+        }
+        pdfMake.createPdf(doc).download("wow.pdf");
+      }
+    });
+  }
+
+  $scope.pdfToPng = function () {
+    var convert = $http.get('/convert-pdf?theUrl=' + $scope.linkUrl);
+    convert.then(function(res){
+      console.log('The backgorund now is a PNG file.');
+      $scope.pngUrl = res.data.imgUrl;
+    })
   }
 
   function activate() {
--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
+var html5pdf = require("html5-to-pdf");
+var PDFImage = require("pdf-image").PDFImage;
+var im = require('imagemagick');
+var fs = require("fs");
 app.use(express.static('./public/'));
+
+function randomText(length){
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(var x=0; x < length; x++){
+    text += possible.charAt(Math.floor(Math.random() * possible.length)+1);
+  }
+  return text;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/convert-pdf', function(req, res) {
+  var fullUrl = req.protocol + '://' + req.get('host');
+  var pdfImage = new PDFImage(req.query.theUrl);
+  pdfImage.pdfFileBaseName = randomText(15);
+  pdfImage.outputDirectory = "./public/pdf-png/";
+  pdfImage.convertPage(0).then(function (imagePath) {
+    imagePath = imagePath.split('/');
+    imagePath.splice(0,1);
+    imagePath = fullUrl + '/' + imagePath.join('/');
+    res.json({imgUrl: imagePath});
+  });
 })
 
 app.listen(port, function () {
