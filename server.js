0aae3d70cf2dab198677e78b03917d2c019beb12
John Huang <little78926@gmail.com>
2016-05-12 00:00:14 -0700

    Serve the default index.html

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var port = 1337;
 
 app.use(express.static('./public/'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+})
 
 app.listen(port, function () {
   console.log('server on port: ' + port);
