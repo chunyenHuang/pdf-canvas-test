d8be801dfa6adf542298e37498f980c6c3cbc2c7
John Huang <little78926@gmail.com>
2016-05-12 00:04:53 -0700

    Edit server port.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 1337;
-
 app.use(express.static('./public/'));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 })
